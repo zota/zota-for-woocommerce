f131f2b32cd88077dd8efbe58659b63af7a4167d
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-08-18 19:10:44 +0300

    Gateway class Zota_WooCommerce prepared

---
+++ includes/class-zota-woocommerce.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Zota for WooCommerce
+ *
+ * @package ZotaWooCommerce
+ * @author  Zota
+ */
+
+use Zota\ZotaWooCommerce\Includes;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Zota_WooCommerce class.
+ *
+ * @extends WC_Mypos_Payment_Gateway
+ */
+class Zota_WooCommerce extends WC_Payment_Gateway {
+}
