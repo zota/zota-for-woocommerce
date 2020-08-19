1e0d50618bd9684fadbdfffc95ef564b42b874e7
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-08-19 11:29:00 +0300

    Payment Gateway required methods added

--- includes/class-zota-woocommerce.php
+++ includes/class-zota-woocommerce.php
@@ -6,7 +6,7 @@
  * @author  Zota
  */
 
-use Zota\ZotaWooCommerce\Includes;
+use Zota\Zota_WooCommerce\Includes\Settings;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -15,7 +15,144 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Zota_WooCommerce class.
  *
- * @extends WC_Mypos_Payment_Gateway
+ * @extends WC_Payment_Gateway
  */
 class Zota_WooCommerce extends WC_Payment_Gateway {
+
+	/**
+	 * Zota Supported currencies
+	 *
+	 * @var array
+	 */
+	public static $supported_currencies = array(
+		'USD',
+		'EUR',
+		'MYR',
+		'VND',
+		'THB',
+		'IDR',
+		'CNY',
+	);
+
+	/**
+	 * Defines main properties, load settings fields and hooks
+	 */
+	public function __construct() {
+
+		$this->id                 = ZOTA_WC_GATEWAY_ID;
+		$this->icon               = ZOTA_WC_URL . 'dist/img/logo.png';
+		$this->has_fields         = false;
+		$this->method_title       = ZOTA_WC_NAME;
+		$this->method_description = esc_html__( 'Add card payments to WooCommerce with Zota', 'zota-woocommerce' );
+		$this->supports           = array(
+			'products',
+			'refunds',
+		);
+		$this->version            = ZOTA_WC_VERSION;
+		$this->title              = $this->get_option( 'title' );
+		$this->description        = $this->get_option( 'description' );
+
+		// Load the form fields.
+		$this->init_form_fields();
+
+		// Load the settings.
+		$this->init_settings();
+
+		// Hooks.
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+	}
+
+	/**
+	 * Get supported currencies
+	 *
+	 * @return array
+	 */
+	public function supported_currencies() {
+		return apply_filters( ZOTA_WC_GATEWAY_ID . '_supported_currencies', self::$supported_currencies );
+	}
+
+	/**
+	 * Check if the currency is in the supported currencies
+	 *
+	 * @return bool
+	 */
+	public function is_supported() {
+		return in_array( get_woocommerce_currency(), $this->supported_currencies(), true );
+	}
+
+	/**
+	 * Check if the gateway is available.
+	 *
+	 * @return false|self
+	 */
+	public function is_available() {
+		if ( ! $this->is_supported() ) {
+			return false;
+		}
+
+		return parent::is_available();
+	}
+
+	/**
+	 * Settings Form Fields
+	 *
+	 * @return void
+	 */
+	public function init_form_fields() {
+		if ( false === $this->is_supported() ) {
+			return;
+		}
+
+		$this->form_fields = Settings::form_fields();
+	}
+
+	/**
+	 * Admin options scripts.
+	 *
+	 * @param  string $hook WooCommerce Hook.
+	 * @return void
+	 */
+	public function admin_enqueue_scripts( $hook ) {
+		if ( 'woocommerce_page_wc-settings' !== $hook ) {
+			return;
+		}
+		wp_enqueue_script( 'zota-woocommerce', ZOTA_WC_URL . '/dist/js/admin.js', array(), ZOTA_WC_VERSION, true );
+	}
+
+	/**
+	 * Admin Panel Options
+	 *
+	 * @return string|self
+	 */
+	public function admin_options() {
+		if ( false === $this->is_supported() ) {
+			?>
+			<div class="inline error">
+				<p>
+					<strong><?php esc_html_e( 'Gateway Disabled', 'zota-woocommerce' ); ?></strong>:
+					<?php esc_html_e( 'Zota does not support your store currency.', 'zota-woocommerce' ); ?>
+				</p>
+			</div>
+			<?php
+			return;
+		}
+
+		parent::admin_options();
+	}
+
+	/**
+	 * Process the payment and return the result
+	 *
+	 * @param  int $order_id Order ID.
+	 * @return array
+	 */
+	public function process_payment( $order_id ) {
+		$order = new WC_Order( $order_id );
+
+		return array(
+			'result'   => 'success',
+			'redirect' => $order->get_checkout_payment_url( true ),
+		);
+	}
 }
