72e35cfba0eaebf6bbed3cc408f17fbd042bea5e
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-08-21 13:15:16 +0300

    Request class integrated, response class hooked to order received page.

--- includes/class-zota-woocommerce.php
+++ includes/class-zota-woocommerce.php
@@ -7,9 +7,9 @@
  */
 
 use \Zota\Zota_WooCommerce\Includes\Settings;
+use \Zota\Zota_WooCommerce\Includes\Zotapay_Request;
+use \Zota\Zota_WooCommerce\Includes\Zotapay_Response;
 use \Zotapay\Zotapay;
-use \Zotapay\DepositOrder;
-use \Zotapay\Deposit;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -38,18 +38,25 @@ class Zota_WooCommerce extends WC_Payment_Gateway {
 	);
 
 	/**
-	 * The url for notifications
-	 *
-	 * @var string
-	 */
-	public $url_notify;
-
-	/**
 	 * The test prefix
 	 *
 	 * @var string
 	 */
 	public $test_prefix;
+
+	/**
+	 * Callback url
+	 *
+	 * @var string
+	 */
+	public $callback_url;
+
+	/**
+	 * The request object
+	 *
+	 * @var string
+	 */
+	public $request;
 
 	/**
 	 * Defines main properties, load settings fields and hooks
@@ -58,7 +65,7 @@ class Zota_WooCommerce extends WC_Payment_Gateway {
 
 		// Initial settings.
 		$this->id                 = ZOTA_WC_GATEWAY_ID;
-		$this->icon               = ZOTA_WC_URL . 'dist/img/logo.png';
+		$this->icon               = ZOTA_WC_URL . 'dist/img/icon.png';
 		$this->has_fields         = false;
 		$this->method_title       = ZOTA_WC_NAME;
 		$this->method_description = esc_html__( 'Add card payments to WooCommerce with Zota', 'zota-woocommerce' );
@@ -66,7 +73,7 @@ class Zota_WooCommerce extends WC_Payment_Gateway {
 			'products',
 		);
 		$this->version            = ZOTA_WC_VERSION;
-		$this->url_notify         = preg_replace( '/^http:/i', 'https:', home_url( '?wc-api=' . $this->id ) );
+		$this->callback_url       = preg_replace( '/^http:/i', 'https:', home_url( '?wc-api=' . $this->id ) );
 
 		// Load the form fields.
 		$this->init_form_fields();
@@ -96,13 +103,14 @@ class Zota_WooCommerce extends WC_Payment_Gateway {
 		Zotapay::setEndpoint( $this->get_option( ( $testmode ? 'test_endpoint_' : 'endpoint_' ) . strtolower( get_woocommerce_currency() ) ) );
 		Zotapay::setApiBase( $testmode ? 'https://api.zotapay-sandbox.com' : 'https://api.zotapay.com' );
 
+		// Setup Zotapay request object.
+		$this->request = new Zotapay_Request( $this );
+
 		// Hooks.
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-
-		if ( $this->is_available() ) {
-			add_action( 'woocommerce_receipt_' . $this->id, array( $this, 'receipt' ) );
-		}
+		add_action( 'woocommerce_api_' . $this->id, array( '\Zota\Zota_WooCommerce\Includes\Zotapay_Response', 'callback' ) );
+		add_action( 'woocommerce_thankyou_' . $this->id, array( '\Zota\Zota_WooCommerce\Includes\Zotapay_Response', 'redirect' ) );
 	}
 
 	/**
@@ -190,35 +198,9 @@ class Zota_WooCommerce extends WC_Payment_Gateway {
 	 * @return array
 	 */
 	public function process_payment( $order_id ) {
+		global $woocommerce;
 
-		$order = wc_get_order( $order_id );
-
-		// Prepare deposit order.
-		$deposit_order = new DepositOrder();
-		$deposit_order->setMerchantOrderID( $this->test_prefix . $order->get_id() );
-		$deposit_order->setMerchantOrderDesc( ZOTA_WC_NAME . '. Order Number: ' . $order->get_order_number() );
-		$deposit_order->setOrderAmount( number_format( $order->get_total(), 2, '.', '' ) );
-		$deposit_order->setOrderCurrency( $order->get_currency() );
-		$deposit_order->setCustomerEmail( $order->get_billing_email() );
-		$deposit_order->setCustomerFirstName( $order->get_billing_first_name() );
-		$deposit_order->setCustomerLastName( $order->get_billing_last_name() );
-		$deposit_order->setCustomerAddress( $order->get_billing_address_1() );
-		$deposit_order->setCustomerCountryCode( $order->get_billing_country() );
-		$deposit_order->setCustomerCity( $order->get_billing_city() );
-		$deposit_order->setCustomerState( '' );
-		$deposit_order->setCustomerZipCode( $order->get_billing_postcode() );
-		$deposit_order->setCustomerPhone( $order->get_billing_phone() );
-		$deposit_order->setCustomerIP( WC_Geolocation::get_ip_address() );
-		$deposit_order->setCustomerBankCode( '' );
-		$deposit_order->setRedirectUrl( $this->url_notify );
-		$deposit_order->setCallbackUrl( $this->url_notify );
-		$deposit_order->setCheckoutUrl( $this->url_notify );
-		$deposit_order->setLanguage( 'EN' );
-
-		// Deposit request.
-		$deposit  = new Deposit();
-		$response = $deposit->request( $deposit_order );
-
+		$response = $this->request->deposit( $order_id );
 		if ( null !== $response->getMessage() ) {
 			wc_add_notice(
 				'Zotapay Error: ' . esc_html( '(' . $response->getCode() . ') ' . $response->getMessage() ),
@@ -227,7 +209,10 @@ class Zota_WooCommerce extends WC_Payment_Gateway {
 			return;
 		}
 
-		// https://wordpress.local/?wc-api=wc_gateway_zota&billingDescriptor=sandbox-payment&merchantOrderID=71222732-test-1917&orderID=24054137&signature=32b4c5162d37ead656009ba1cab95f35afaf72625735106c0dc502088e2d0c5b&status=APPROVED
+		// Remove cart.
+		$woocommerce->cart->empty_cart();
+
+		// TODO add expiration time.
 
 		return array(
 			'result'   => 'success',
