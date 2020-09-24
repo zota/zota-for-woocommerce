5461854df8ca2d14983f46cde5abe7aa329eb910
Atanas <29710963+atanasantonov@users.noreply.github.com>
2020-09-24 19:51:45 +0300

    Deleting all stored settings to clean sensitive data on uninstall.

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php  // phpcs:ignore XSS ok
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// If uninstall not called from WordPress exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Delete options.
+delete_option( 'woocommerce_wc_gateway_zota_settings' );
