fd0da9889cbdb1d0096038240cd78ac27082dd07
Milen Petrinski <gonzo@greatgonzo.net>
2021-01-12 11:41:18 +0200

    First passing tests.

--- tests/test-zota-woocommerce.php
+++ tests/test-zota-woocommerce.php
@@ -13,15 +13,25 @@ class WC_Tests_Zota_WooCommerce extends WC_Unit_Test_Case {
 	 */
 	public function setUp() {
 		parent::setUp();
+
+		/**
+		 * TODO: We should probably move these in a global helper / setUp method.
+		 */
+		update_option( 'woocommerce_currency', 'USD' );
+
+		$settings = [
+			'testmode' => 'yes',
+			'test_merchant_id' => 'dummy_merchant_id',
+			'test_merchant_secret_key' => 'dummy_merchant_secret_key',
+			'test_endpoint_' . strtolower( get_woocommerce_currency() ) => 'dummy_endpoint',
+			'enabled' => 'yes',
+		];
+		update_option( 'woocommerce_' . ZOTA_WC_GATEWAY_ID . '_settings', $settings );
+
 		WC()->session = null;
+
 		$wc_payment_gateways = WC_Payment_Gateways::instance();
 		$wc_payment_gateways->init();
-		var_dump( array_keys( $wc_payment_gateways->payment_gateways() ) );
-		foreach ( $wc_payment_gateways->payment_gateways() as $name => $gateway ) {
-			if ( in_array( $name, array( ZOTA_WC_GATEWAY_ID ) ) ) {
-				$gateway->enabled = 'yes';
-			}
-		}
 	}
 
 	/**
@@ -33,6 +43,20 @@ class WC_Tests_Zota_WooCommerce extends WC_Unit_Test_Case {
 	}
 
 	/**
+	 * Test that Zota payment gateway is available.
+	 */
+	public function test_gateway_available() {
+		WC()->initialize_session();
+		wp_set_current_user( 1 );
+
+		$gateways = WC()->payment_gateways()->payment_gateways();
+
+		$this->assertFalse( empty( $gateways[ ZOTA_WC_GATEWAY_ID ] ) );
+		$this->assertInstanceOf( 'Zota_WooCommerce', $gateways[ ZOTA_WC_GATEWAY_ID ] );
+		$this->assertTrue( $gateways[ ZOTA_WC_GATEWAY_ID ]->is_available() );
+	}
+
+	/**
 	 * Test that Zota payment gateway is enabled.
 	 */
 	public function test_gateway_enabled() {
@@ -40,7 +64,9 @@ class WC_Tests_Zota_WooCommerce extends WC_Unit_Test_Case {
 		wp_set_current_user( 1 );
 
 		$gateways = WC()->payment_gateways()->get_available_payment_gateways();
-		var_dump( $gateways );
+
 		$this->assertFalse( empty( $gateways[ ZOTA_WC_GATEWAY_ID ] ) );
+		$this->assertInstanceOf( 'Zota_WooCommerce', $gateways[ ZOTA_WC_GATEWAY_ID ] );
 	}
+
 }
