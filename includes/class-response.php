48cb5f9ca8f03f606f62b95ecc154f6c3eccde22
user <29710963+atanasantonov@users.noreply.github.com>
2021-02-12 12:44:52 +0200

    Multiple payment attempts for order handled.

--- includes/class-response.php
+++ includes/class-response.php
@@ -27,6 +27,8 @@ class Response {
 	 * Zotapay callback.
 	 */
 	public static function callback() {
+		Zotapay::getLogger()->info( esc_html__( 'Callback received.', 'zota-woocommerce' ) );
+
 		try {
 			// Get the callback handler.
 			$callback = new ApiCallback();
@@ -39,11 +41,11 @@ class Response {
 				wp_send_json_error( $error, 400 );
 			}
 
-			// Remove test prefix.
+			// Remove test suffix.
 			$order_id = Order::remove_uniqid_suffix( $callback->getMerchantOrderID() );
 
+			// Get WC Order.
 			$order = wc_get_order( $order_id );
-
 			if ( empty( $order ) ) {
 				$error = sprintf(
 					// translators: %1$s order ID.
@@ -57,11 +59,22 @@ class Response {
 			Zotapay::getLogger()->info(
 				sprintf(
 					// translators: %1$s Order ID, %2$s Merchant Order ID.
-					esc_html__( 'Callback Order #%1$s / Merchant Order ID \w test prefix #%2$s', 'zota-woocommerce' ),
+					esc_html__( 'Callback for Order #%1$s / Merchant Order ID \w test prefix #%2$s', 'zota-woocommerce' ),
 					$order_id,
 					$callback->getMerchantOrderID()
 				)
 			);
+
+			// Check Order ID.
+			if ( null === $callback->getOrderID() ) {
+				$error = sprintf(
+					// translators: %1$s Merchant Order ID.
+					esc_html__( 'Merchant Order ID %1$s no Order ID.', 'zota-woocommerce' ),
+					$callback->getMerchantOrderID()
+				);
+				Zotapay::getLogger()->error( $error );
+				wp_send_json_error( $error, 400 );
+			}
 
 			// Check Status.
 			if ( null === $callback->getStatus() ) {
@@ -74,22 +87,33 @@ class Response {
 				wp_send_json_error( $error, 400 );
 			}
 
+			// Check if is the last request by matching last stored ZotaPay order ID.
+			$zotapay_order_id = $order->get_meta( '_zotapay_order_id', true );
+			if ( $callback->getOrderID() !== $zotapay_order_id ) {
+				$note = sprintf(
+					// translators: %s WC Order ID.
+					esc_html__( 'Callback from previous payment attempt with ZotaPay status: %s.', 'zota-woocommerce' ),
+					$callback->getStatus()
+				);
+				$order->add_order_note( $note );
+				$order->save();
+
+				Zotapay::getLogger()->info(
+					sprintf(
+						// translators: %1$s Order ID, %2$s Merchant Order ID, %3$s ZotaPay status.
+						esc_html__( 'Callback from previous payment for Order #%1$s / Merchant Order ID #%2$s, ZotaPay status: %3$s', 'zota-woocommerce' ),
+						$order_id,
+						$callback->getMerchantOrderID()
+					)
+				);
+				wp_send_json_success();
+			}
+
 			// Check Processor Transaction ID.
 			if ( null === $callback->getProcessorTransactionID() ) {
 				$error = sprintf(
 					// translators: %1$s Merchant Order ID.
 					esc_html__( 'Merchant Order ID %1$s no Processor Transaction ID.', 'zota-woocommerce' ),
-					$callback->getMerchantOrderID()
-				);
-				Zotapay::getLogger()->error( $error );
-				wp_send_json_error( $error, 400 );
-			}
-
-			// Check Order ID.
-			if ( null === $callback->getOrderID() ) {
-				$error = sprintf(
-					// translators: %1$s Merchant Order ID.
-					esc_html__( 'Merchant Order ID %1$s no Order ID.', 'zota-woocommerce' ),
 					$callback->getMerchantOrderID()
 				);
 				Zotapay::getLogger()->error( $error );
@@ -124,6 +148,8 @@ class Response {
 	 * @return void
 	 */
 	public static function redirect( $order_id ) {
+		Zotapay::getLogger()->info( esc_html__( 'Merchant redirect.', 'zota-woocommerce' ) );
+
 		$order = wc_get_order( $order_id );
 
 		// If redirect is processed do nothing.
@@ -141,6 +167,20 @@ class Response {
 
 			// Check if order ids matching.
 			if ( $order_id !== $merchant_order_id ) {
+				return;
+			}
+
+			// Check if is the last request by matching last stored ZotaPay order ID.
+			$zotapay_order_id = $order->get_meta( '_zotapay_order_id', true );
+			if ( $redirect->getOrderID() !== $zotapay_order_id ) {
+				Zotapay::getLogger()->info(
+					sprintf(
+						// translators: %1$s Order ID, %2$s Merchant Order ID, %3$s ZotaPay status.
+						esc_html__( 'Redirect from previous payment for Order #%1$s / Merchant Order ID #%2$s, ZotaPay status: %3$s', 'zota-woocommerce' ),
+						$order_id,
+						$redirect->getMerchantOrderID()
+					)
+				);
 				return;
 			}
 
