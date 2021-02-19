8c04fcff515df02233ee45a8e2a5c408d1ced299
user <29710963+atanasantonov@users.noreply.github.com>
2021-02-19 17:51:20 +0200

    phpcs

--- uninstall.php
+++ uninstall.php
@@ -1,13 +1,13 @@
-<?php  // phpcs:ignore XSS ok
+<?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 // If uninstall not called from WordPress exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
 }
 
 // Delete options.
-delete_option( 'woocommerce_wc_gateway_zota_settings' );
+delete_option('woocommerce_wc_gateway_zota_settings');
