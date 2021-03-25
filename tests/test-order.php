d4c8ac7b5db912b8fa89c4d00e503086e02dc381
user <29710963+atanasantonov@users.noreply.github.com>
2021-03-25 14:38:57 +0200

    Add test for Order::get_total_paid()

--- tests/test-order.php
+++ tests/test-order.php
@@ -73,7 +73,7 @@ class WC_Tests_Order extends WC_Unit_Test_Case {
 	 *
 	 * @dataProvider getDataPartialPayment
 	 */
-	public function amount_changed_partial($data) {
+	public function test_amount_changed_partial($data) {
 		WC()->initialize_session();
 
 		$payment_gateways = WC()->payment_gateways->payment_gateways();
@@ -85,5 +85,62 @@ class WC_Tests_Order extends WC_Unit_Test_Case {
 		$is_amount_changed = \Zota\Zota_WooCommerce\Includes\Order::amount_changed($callback);
 
 		$this->assertTrue( $is_amount_changed );
+	}
+
+
+    /**
+	 * Test get order total paid for single callback.
+	 */
+	public function test_get_total_paid_for_single_callback() {
+		WC()->initialize_session();
+
+        $product = WC_Helper_Product::create_simple_product(
+			true,
+			[
+				'tax_status' => 'none',
+				'virtual' => true,
+			]
+		);
+
+        $order = WC_Helper_Order::create_order(true, $product, $this->payment_method);
+        $order->save();
+
+        // Single callback.
+        $paid = "5.00";
+        \Zota\Zota_WooCommerce\Includes\Order::handle_amount_changed($order, $paid, $order->get_total());
+
+		$total_paid = \Zota\Zota_WooCommerce\Includes\Order::get_total_paid($order);
+
+		$this->assertSame($total_paid, \floatval( $paid ));
+	}
+
+    /**
+	 * Test get order total paid for multiple callbacks.
+	 */
+	public function test_get_total_paid_for_multiple_callbacks() {
+		WC()->initialize_session();
+
+        $product = WC_Helper_Product::create_simple_product(
+			true,
+			[
+                'regular_price' => 100,
+				'price'         => 100,
+				'tax_status' => 'none',
+				'virtual' => true,
+			]
+		);
+
+        $order = WC_Helper_Order::create_order(true, $product, $this->payment_method);
+        $order->save();
+
+        // First callback
+        \Zota\Zota_WooCommerce\Includes\Order::handle_amount_changed($order, "20.00", "100.00");
+
+        // Second callback
+        \Zota\Zota_WooCommerce\Includes\Order::handle_amount_changed($order, "15.00", "100.00");
+
+		$total_paid = \Zota\Zota_WooCommerce\Includes\Order::get_total_paid($order);
+        echo
+		$this->assertSame($total_paid, 35.00);
 	}
 }
