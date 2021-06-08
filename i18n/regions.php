e6397aa0d5ec79ddbeafd617037345dee2509d8c
user <29710963+atanasantonov@users.noreply.github.com>
2021-06-08 17:12:50 +0300

    Countries by region array prepared.

---
+++ i18n/regions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Regions.
+ *
+ * @package ZotaWooCommerce
+ * @author  Zota
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+return array(
+	'AF' => array(
+		'name'      => __( 'Africa', 'woocommerce' ),
+		'countries' => array(
+
+		),
+	)
+);
--- templates/field-countries.php
+++ templates/field-countries.php
@@ -10,6 +10,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$wc_countries = new WC_Countries();
+wc_print_r( $wc_countries->get_countries() );
+
 ?>
 <tr valign="top">
 	<th scope="row" class="titledesc">
@@ -25,7 +28,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 			value="<?php echo esc_attr( $value['value'] ); ?>"
 			>
 		<div class="checkbox-list">
-
+			<label for="woocommerce_enable_myaccount_registration">
+				<input name="woocommerce_enable_myaccount_registration" id="woocommerce_enable_myaccount_registration" type="checkbox" class="" value="1" checked="checked">
+				Позволяване на клиентите да създадат профил на страницата "Моят профил"
+			</label>
 		</div>
 	</td>
 </tr>
